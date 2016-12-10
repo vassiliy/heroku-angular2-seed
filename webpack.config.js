92f95c666bbe6dbf91cf8f9bcf1ad4c0ba8b9348
Vassiliy Pimkin <vassiliy.pimkin@gmail.com>
2016-12-10 15:42:01 +0300

    Caught up with the official `angular/angular2-seed`

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,81 @@ if (!production) { require('dotenv').config(); }
 
 var webpack = require('webpack');
 var path = require('path');
-
+var webpackMerge = require('webpack-merge');
 
 // Webpack Config
 var webpackConfig = {
   entry: {
-    'polyfills': './src/polyfills.browser.ts',
-    'vendor':    './src/vendor.browser.ts',
-    'main':       './src/main.browser.ts',
+    'main': './src/main.browser.ts',
   },
 
   output: {
-    path: './dist',
+    publicPath: '',
+    path: path.resolve(__dirname, './dist'),
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(true),
-    new webpack.optimize.CommonsChunkPlugin({ name: ['main', 'vendor', 'polyfills'], minChunks: Infinity }),
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+      path.resolve(__dirname, './src'),
+      {
+        // your Angular Async Route paths relative to this root directory
+      }
+    ),
     new webpack.EnvironmentPlugin([
       'CONFIG_VAR', 'NODE_ENV'
-      ]),
+      ])
   ],
 
   module: {
     loaders: [
       // .ts files for TypeScript
-      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
+      {
+        test: /\.ts$/,
+        loaders: [
+          'awesome-typescript-loader',
+          'angular2-template-loader',
+          'angular2-router-loader'
+        ]
+      },
       { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
       { test: /\.html$/, loader: 'raw-loader' }
     ]
   }
 
+};
+
+// Our Webpack Defaults
+var defaultConfig = {
+  devtool: 'source-map',
+
+  output: {
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+    modules: [ path.resolve(__dirname, 'node_modules') ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 }
+  },
+
+  node: {
+    global: true,
+    crypto: 'empty',
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
 };
 
 if (production) {
@@ -47,36 +91,4 @@ if (production) {
     }}));
 }
 
-// Our Webpack Defaults
-var defaultConfig = {
-  devtool: 'cheap-module-source-map',
-  cache: true,
-  debug: true,
-  output: {
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js'
-  },
-
-  resolve: {
-    root: [ path.join(__dirname, 'src') ],
-    extensions: ['', '.ts', '.js']
-  },
-
-  devServer: {
-    historyApiFallback: true,
-    watchOptions: { aggregateTimeout: 300, poll: 1000 }
-  },
-
-  node: {
-    global: 1,
-    crypto: 'empty',
-    module: 0,
-    Buffer: 0,
-    clearImmediate: 0,
-    setImmediate: 0
-  }
-};
-
-var webpackMerge = require('webpack-merge');
 module.exports = webpackMerge(defaultConfig, webpackConfig);
